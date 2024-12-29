c5e02d342a516cf19050a75f1e29c9690c4f915e
rachit74 <rachithooda09@gmail.com>
2024-12-29 18:04:36 +0530

    linked list insertion in the middle

--- java/linked_list/LinkedList.java
+++ java/linked_list/LinkedList.java
@@ -35,6 +35,34 @@ public class LinkedList {
         }
     }
 
+    // insertInMiddle
+    /*
+     -> determine position to insert new node at
+     -> check if the position is in the range
+     -> if position is 0 or n then call requried of the two functions
+     -> go to position-1 node, set newNode.next = temp.next, and then temp.next=newNode
+     */
+    public void insertInMiddle(int data, int position) {
+        int n = size();
+        if (position < 0 || position > n) {
+            System.err.println("Position out of range");
+        } if (position == n) {
+            insertAtEnd(data);
+        } if (position == 0) {
+            insertAtBeginning(data);
+        } else {
+            // creating new Node
+            Node newNode = new Node(data);
+            // handle inserting in middle
+            Node temp = head;
+            for (int i = 0; i < position - 1; i ++ ) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
     // traverse the list
     public void traverse(){
         // check if list is empty
@@ -49,6 +77,20 @@ public class LinkedList {
         }
     }
 
+    public int size() {
+        int n = 0;
+        if (head == null) {
+            return 0;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+                n = n + 1;
+            }
+        }
+        return n;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.traverse();
@@ -59,6 +101,7 @@ public class LinkedList {
         list.insertAtEnd(70);
         list.insertAtEnd(80);
         list.insertAtEnd(90);
+        list.insertInMiddle(120, 3);
         list.traverse();
     }
 
